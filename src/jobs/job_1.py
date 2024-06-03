9bfedb344bc3fda82a861e98aeeea20a17e688a8
Aleem Rahil <aleem.rahil@outlook.com>
2024-06-03 14:20:14 -0400

    Update job_1.py
    completed job1

--- src/jobs/job_1.py
+++ src/jobs/job_1.py
@@ -4,7 +4,25 @@ from pyspark.sql.dataframe import DataFrame
 
 def query_1(output_table_name: str) -> str:
     query = f"""
-    <YOUR QUERY HERE>
+    WITH lagged AS (
+        SELECT player_name,
+          CASE WHEN is_active THEN 1 ELSE 0 END AS is_active,
+          current_season,
+          CASE WHEN LAG(is_active,1) OVER (PARTITION BY player_name ORDER BY current_season) THEN 1 ELSE 0 END AS is_active_last_season
+        FROM {input_table_name}
+        WHERE current_season <= 2015
+        ), streaked AS (
+          SELECT *,
+            SUM(CASE WHEN is_active <> is_active_last_season THEN 1 ELSE 0 END) OVER (PARTITION BY player_name ORDER BY current_season) AS streak_identifier
+          FROM lagged
+        )
+        SELECT player_name,
+          MAX(is_active) = 1 AS is_active,
+          MIN(current_season) AS start_season,
+          MAX(current_season) AS end_season
+        FROM streaked
+        GROUP BY player_name,
+          streak_identifier
     """
     return query
 
@@ -14,12 +32,14 @@ def job_1(spark_session: SparkSession, output_table_name: str) -> Optional[DataF
   return spark_session.sql(query_1(output_table_name))
 
 def main():
-    output_table_name: str = "<output table name here>"
+    input_table_name: str = "nba_players"
+    output_table_name: str = "nba_player_scd_merge"
     spark_session: SparkSession = (
         SparkSession.builder
         .master("local")
         .appName("job_1")
         .getOrCreate()
     )
+    input_df = spark_session.table(input_table_name)
     output_df = job_1(spark_session, output_table_name)
     output_df.write.mode("overwrite").insertInto(output_table_name)
